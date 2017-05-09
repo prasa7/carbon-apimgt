5913c47c9ea92e0904519d697f465060da737707
Prasanna Dangalla <prasannadangalla@gmail.com>
2017-05-09 07:01:00 +0530

    Added AdvancedThrottlePolicyMappingUtil, ApplicationThrottlePolicyMappingUtil, CommonThrottleMappingUtil, SubscriptionThrottlePolicyMappingUtil and the respective changes.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/gen/java/org/wso2/carbon/apimgt/rest/api/admin/PoliciesApiService.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/gen/java/org/wso2/carbon/apimgt/rest/api/admin/PoliciesApiService.java
@@ -7,11 +7,13 @@ import org.wso2.msf4j.formparam.FormDataParam;
 import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.Request;
 
-import org.wso2.carbon.apimgt.rest.api.admin.dto.CustomRuleDTO;
-import org.wso2.carbon.apimgt.rest.api.admin.dto.CustomRuleListDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.AdvancedThrottlePolicyDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.AdvancedThrottlePolicyListDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.ApplicationThrottlePolicyDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.ApplicationThrottlePolicyListDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.dto.ErrorDTO;
-import org.wso2.carbon.apimgt.rest.api.admin.dto.TierDTO;
-import org.wso2.carbon.apimgt.rest.api.admin.dto.TierListDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.SubscriptionThrottlePolicyDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.SubscriptionThrottlePolicyListDTO;
 
 import java.util.List;
 import org.wso2.carbon.apimgt.rest.api.admin.NotFoundException;
@@ -21,7 +23,7 @@ import java.io.InputStream;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-@javax.annotation.Generated(value = "org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-05-04T16:42:24.822+05:30")
+@javax.annotation.Generated(value = "org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-05-08T20:35:18.344+05:30")
 public abstract class PoliciesApiService {
     public abstract Response policiesThrottlingAdvancedGet(String accept
  ,String ifNoneMatch
@@ -36,12 +38,12 @@ public abstract class PoliciesApiService {
  ,String ifModifiedSince
  , Request request) throws NotFoundException;
     public abstract Response policiesThrottlingAdvancedPolicyIdPut(String policyId
- ,TierDTO body
+ ,AdvancedThrottlePolicyDTO body
  ,String contentType
  ,String ifMatch
  ,String ifUnmodifiedSince
  , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingAdvancedPost(TierDTO body
+    public abstract Response policiesThrottlingAdvancedPost(AdvancedThrottlePolicyDTO body
  ,String contentType
  , Request request) throws NotFoundException;
     public abstract Response policiesThrottlingApplicationGet(String accept
@@ -57,34 +59,13 @@ public abstract class PoliciesApiService {
  ,String ifModifiedSince
  , Request request) throws NotFoundException;
     public abstract Response policiesThrottlingApplicationPolicyIdPut(String policyId
- ,TierDTO body
+ ,ApplicationThrottlePolicyDTO body
  ,String contentType
  ,String ifMatch
  ,String ifUnmodifiedSince
  , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingApplicationPost(TierDTO body
+    public abstract Response policiesThrottlingApplicationPost(ApplicationThrottlePolicyDTO body
  ,String contentType
- , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingCustomGet(String accept
- ,String ifNoneMatch
- ,String ifModifiedSince
- , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingCustomPost(CustomRuleDTO body
- ,String contentType
- , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingCustomRuleIdDelete(String ruleId
- ,String ifMatch
- ,String ifUnmodifiedSince
- , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingCustomRuleIdGet(String ruleId
- ,String ifNoneMatch
- ,String ifModifiedSince
- , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingCustomRuleIdPut(String ruleId
- ,CustomRuleDTO body
- ,String contentType
- ,String ifMatch
- ,String ifUnmodifiedSince
  , Request request) throws NotFoundException;
     public abstract Response policiesThrottlingSubscriptionGet(String accept
  ,String ifNoneMatch
@@ -99,12 +80,12 @@ public abstract class PoliciesApiService {
  ,String ifModifiedSince
  , Request request) throws NotFoundException;
     public abstract Response policiesThrottlingSubscriptionPolicyIdPut(String policyId
- ,TierDTO body
+ ,SubscriptionThrottlePolicyDTO body
  ,String contentType
  ,String ifMatch
  ,String ifUnmodifiedSince
  , Request request) throws NotFoundException;
-    public abstract Response policiesThrottlingSubscriptionPost(TierDTO body
+    public abstract Response policiesThrottlingSubscriptionPost(SubscriptionThrottlePolicyDTO body
  ,String contentType
  , Request request) throws NotFoundException;
 }
