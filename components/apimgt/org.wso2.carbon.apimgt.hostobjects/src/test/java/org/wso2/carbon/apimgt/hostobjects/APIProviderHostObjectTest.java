c44b7e95a868273275663e50b8b4343336b349d1
Tharindu Dharmarathne <tharindua@wso2.com>
2017-10-02 11:46:04 +0530

    commit test cases on Oauthauthenticator,KeyTemplateRetriever

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/test/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObjectTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/test/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObjectTest.java
@@ -17,18 +17,20 @@
 */
 package org.wso2.carbon.apimgt.hostobjects;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.wso2.carbon.apimgt.api.APIManagementException;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 import java.io.File;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.head;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 /**
  * APIProvider Test Suite
