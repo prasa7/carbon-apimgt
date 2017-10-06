e0ce103248eddc2c8e7d72c07c5e028cfc2d00e2
Praminda Jayawardana <pramindabj@gmail.com>
2017-10-06 18:09:49 +0530

    tests: Add impl.internal unit tests
    Only initial APIManagerComponent tests cases included in this commit

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/internal/APIManagerComponent.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/internal/APIManagerComponent.java
@@ -521,8 +521,7 @@ public class APIManagerComponent {
      * This method will create new permission name  "applications" in registry permission.
      */
     private void addApplicationsPermissionsToRegistry() throws APIManagementException {
-        Registry tenantGovReg = CarbonContext.getThreadLocalCarbonContext().getRegistry(
-                RegistryType.USER_GOVERNANCE);
+        Registry tenantGovReg = getRegistry();
 
         String permissionResourcePath = CarbonConstants.UI_PERMISSION_NAME + RegistryConstants.PATH_SEPARATOR +
                                         APPLICATION_ROOT_PERMISSION;
@@ -552,6 +551,10 @@ public class APIManagerComponent {
 
     }
 
+    protected Registry getRegistry() {
+        return CarbonContext.getThreadLocalCarbonContext().getRegistry(
+                RegistryType.USER_GOVERNANCE);
+    }
 
     private void configureJMSPublisher() {
         OutputEventAdapterConfiguration adapterConfiguration = new OutputEventAdapterConfiguration();
