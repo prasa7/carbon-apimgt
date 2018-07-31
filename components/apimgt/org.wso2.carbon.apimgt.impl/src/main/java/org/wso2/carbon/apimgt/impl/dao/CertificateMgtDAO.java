f0437b082e8141d19135d83f1b548182fe80775e
Menaka Jayawardena <menaka@wso2.com>
2018-07-31 18:26:28 +0530

    Fix test cases.

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/CertificateMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/CertificateMgtDAO.java
@@ -291,7 +291,6 @@ public class CertificateMgtDAO {
             preparedStatement = connection.prepareStatement(deleteCertQuery);
             preparedStatement.setInt(1, tenantId);
             preparedStatement.setString(2, alias);
-            preparedStatement.setString(3, endpoint);
             result = preparedStatement.executeUpdate() == 1;
             connection.commit();
             connection.setAutoCommit(initialAutoCommit);
